19581c980abce32d2ff6e6ad5cc7ccf48334aaca
nickgu <gu18168@gmail.com>
2019-08-16 23:29:54 +0800

    :lipstick: improve some search UI/UX

--- scripts/app/main.js
+++ scripts/app/main.js
@@ -1,9 +1,8 @@
 define(function (require) {
     let mapFns = require("./map");
-    let mapBox = require("./mapbox");
+    let mapBoxFns = require("./mapbox");
 
     let map = mapFns.getMap("map");  
-    map.addControl(mapBox.geocoderControl("mapbox.places", {
-        keepOpen: true
-    }));
+
+    mapBoxFns.addGeoContorl(map);
 });
--- scripts/app/mapbox.js
+++ scripts/app/mapbox.js
@@ -1,3 +1,27 @@
+let addGeoContorl = function (mapBox) {
+    return function (map) {
+        let control = mapBox.geocoderControl("mapbox.places", {
+            keepOpen: true
+        });
+
+        map.addControl(control);
+
+        let controlInput = document
+            .querySelector(".leaflet-control-mapbox-geocoder-form input");
+        let controlToggle = document
+            .querySelector(".leaflet-control-mapbox-geocoder-toggle");
+            
+        controlInput.addEventListener("focus", 
+            function () {
+                controlToggle.classList.add("is-active");
+            });
+        controlInput.addEventListener("blur",
+            function () {
+                controlToggle.classList.remove("is-active");
+            });
+    }
+};
+
 define(function (require) {
     let tokens = require("./token");
     require("mapbox");
@@ -5,5 +29,8 @@ define(function (require) {
     // 加载 mapbox 的 token
     L.mapbox.accessToken = tokens.mapbox;
 
-    return L.mapbox;
+    return { 
+        mapbox: L.mapbox, 
+        addGeoContorl: addGeoContorl(L.mapbox)
+    };
 });
