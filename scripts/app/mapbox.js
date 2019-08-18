891ede46888293cb0fba9e6c095c8dc2efcedc41
nickgu <gu18168@gmail.com>
2019-08-18 19:56:36 +0800

    :sparkls: select search item will add a marker

--- scripts/app/mapbox.js
+++ scripts/app/mapbox.js
@@ -2,9 +2,32 @@ let addGeoControl = function (mapBox, injects) {
     // 侵入 GeocoderControl 使其更符合要求
     injects.geoControl();
 
+    // 单例模式，利用 JS 的闭包特性，实现只有一个搜索标记移动
+    let markerFlag = false;
+    let searchMarker = L.marker([-360, -360], {
+        icon: addIcon(mapBox)
+    }); 
+
     return function (map) {
         let control = mapBox.geocoderControl("mapbox.places", {
             keepOpen: true
+        });
+
+        if (!markerFlag) {
+            // 点击标记会使其移动到中心
+            searchMarker.on("click", function (e) {
+                map.panTo(e.target.getLatLng());
+            });
+
+            // 添加到地图上
+            searchMarker.addTo(map);
+            markerFlag = true;
+        }
+
+        control.on("select", function ({ feature }) {
+            // 只需移动搜索标记即可
+            let [lon, lat] = feature.center
+            searchMarker.setLatLng([lat, lon]);
         });
 
         map.addControl(control);
@@ -22,7 +45,15 @@ let addGeoControl = function (mapBox, injects) {
             function () {
                 controlToggle.classList.remove("is-active");
             });
-    }
+    };
+};
+
+let addIcon = function (mapBox, symbol = "circle", size = "medium", color = "#0052CC") {
+    return mapBox.marker.icon({
+        'mark-size': size,
+        'marker-symbol': symbol,
+        'marker-color': color
+    });
 };
 
 define(function (require) {
