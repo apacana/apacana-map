29379b6e0f9283824e727c3f9b8d2f8e6a0e599e
卢鹏宇 <lupengyu.office@bytedance.com>
2019-09-11 23:58:36 +0800

    feat: 添加行程点集点击跳转与搜索结果去重
    Change-Id: Ic237629bc0ef520bb43313529f72faac61323628

--- scripts/app/mapbox.js
+++ scripts/app/mapbox.js
@@ -1,6 +1,14 @@
-pointPopupList = [];
 markerFlag = false;
 userMarkers = [];
+searchMarkets = null;
+
+let initSearchMarket = function (mapBox) {
+    searchMarkets = [0,0,0,0,0].map(() => {
+        return L.marker([-360, -360], {
+            icon: addIcon(mapBox)
+        })
+    });
+};
 
 let addGeoControl = function (mapBox, injects) {
     // 单例模式，利用 JS 的闭包特性，实现只有一个搜索标记移动
@@ -19,7 +27,7 @@ let addGeoControl = function (mapBox, injects) {
             });
             if (!markerFlag) {
                 // 点击标记会使其移动到中心
-                otherMarkers.forEach(item => {
+                searchMarkets.forEach(item => {
                     item.on("click", function (e) {
                         map.panTo(e.target.getLatLng());
                     }).addTo(map);
@@ -30,11 +38,16 @@ let addGeoControl = function (mapBox, injects) {
 
             control.on("show", function ({ features }) {
                 features.forEach((item, index) => {
+                    // 去重
+                    searchMarkets[index]["options"]["point_id"] = item.id;
+                    searchMarkets[index]["options"]["point_type"] = "search_point";
+                    if (!allowAddSearchMarket(item.id)) {
+                        return true // continue
+                    }
                     // 只需移动搜索标记即可
                     let [lon, lat] = item.center;
-                    console.log([lon, lat]);
-                    otherMarkers[index].setLatLng([lat, lon]);
-                    otherMarkers[index].bindPopup(
+                    searchMarkets[index].setLatLng([lat, lon]);
+                    searchMarkets[index].bindPopup(
                         `
                             <p class="leaflet-info-window-name">${item.text}</p>
                             <p class="leaflet-info-window-address">${item.place_name}</p>
@@ -43,7 +56,7 @@ let addGeoControl = function (mapBox, injects) {
                                     <i class="leaflet-info-window-icon icon-loc"></i>
                                     ${lat.toFixed(6)}, ${lon.toFixed(6)}
                                 </p>
-                                <a class="leaflet-info-window-btn" onclick="addPoint('${item.id}', '${item.escp_text}', '${item.escp_place_name}', '${item.center}');">
+                                <a class="leaflet-info-window-btn" onclick="addPoint('${index}', '${item.id}', '${item.escp_text}', '${item.escp_place_name}', '${item.center}');">
                                     <i class="leaflet-info-window-icon icon-add"></i>
                                     添加到点集
                                 </>
@@ -54,11 +67,14 @@ let addGeoControl = function (mapBox, injects) {
             });
 
             control.on("select", function ({ index }) {
-                console.log(index);
-                otherMarkers[index].openPopup();
+                if (!allowAddSearchMarket(searchMarkets[index]["options"]["point_id"], searchMarkets[index]["options"]["point_type"])) {
+                    selectUserMarket(searchMarkets[index]["options"]["point_id"], searchMarkets[index]["options"]["point_type"]);
+                } else {
+                    searchMarkets[index].openPopup();
+                }
             });
 
-            map.addControl(control); 
+            map.addControl(control);
        })
     };
 };
@@ -71,7 +87,8 @@ let addIcon = function (mapBox, symbol = "circle", size = "medium", color = "#00
     });
 };
 
-let addPoint = function(point_id, text, place_name, center, point_type = 'search_point') {
+let addPoint = function(index, point_id, text, place_name, center, point_type = 'search_point') {
+    console.log(index, searchMarkets[index]);
     text = unescape(text);
     place_name = unescape(place_name);
     fetch(requestConfig.domain + requestConfig.addPoint, {
@@ -85,17 +102,58 @@ let addPoint = function(point_id, text, place_name, center, point_type = 'search
     }).then(response => {
         return response.json();
     }).then(data => {
-        console.log(data)
-        // todo: 数据存储展示
+        console.log(data);
+        if (data.code !== 0) {
+            console.log("addPoint failed, code:", data.code);
+        } else {
+            searchMarketToUserMarket(index);
+        }
     }).catch(function(e) {
         console.log("addPoint error");
     });
 };
 
+let searchMarketToUserMarket = function(index) {
+    let market = searchMarkets[index];
+    market.bindPopup(`
+                <p class="leaflet-info-window-name">${point["text"]}</p>
+                <p class="leaflet-info-window-address">${point["place_name"]}</p>
+                <div class="leaflet-info-window-btns">
+                    <p class="leaflet-info-window-latlon">
+                        <i class="leaflet-info-window-icon icon-loc"></i>
+                        ${lat.toFixed(6)}, ${lon.toFixed(6)}
+                    </p>
+                    <a class="leaflet-info-window-btn">
+                        <i class="leaflet-info-window-icon icon-add"></i>
+                        已添加该点
+                    </>
+                </div>
+                `);
+    userMarkers.push(marker);
+    console.log(userMarkers);
+};
+
+selectUserMarket = function(point_id, point_type) {
+    for(let point of userMarkers) {
+        if (point["options"]["point_id"] === point_id && point["options"]["point_type"] === point_type) {
+            point.openPopup();
+        }
+    }
+};
+
+let allowAddSearchMarket = function(point_id, point_type = 'search_point') {
+    for(let point of userMarkers) {
+        if (point["options"]["point_id"] === point_id && point["options"]["point_type"] === point_type) {
+            return false;
+        }
+    }
+    return true;
+};
+
 setUserMarket = function (pointList) {
     userMarkers = [];
     for(let point of pointList) {
-        let marker = L.marker([-360, -360], {icon: addIcon(L.mapbox)});
+        let marker = L.marker([-360, -360], {icon: addIcon(L.mapbox), point_id: point["point_id"], point_type: point["point_type"]});
         marker.addTo(map);
         let [lon, lat] = point["center"].split(",");
         marker.setLatLng([lat, lon]);
@@ -129,6 +187,8 @@ define(function (require) {
 
     // 加载关于 mapbox 的侵入
     let mapboxInjects = require("./mapbox-inject/index");
+
+    initSearchMarket(L.mapbox);
 
     return { 
         addGeoControl: addGeoControl(L.mapbox, mapboxInjects)
--- scripts/app/user.js
+++ scripts/app/user.js
@@ -28,30 +28,31 @@ let makeGetUserInfoRequest = function() {
             console.log(userInfoMem);
             if (data.data["strokes_info"] == null) {
                 // 没有行程信息
-                let pane = "<div id=\"featurelist-titlebar-container\" style=\"height: 763px\">\n" +
-                    "                    <div id=\"featurelist-title-bar\" class=\"featurelist-title-barClass\">\n" +
-                    "                        <div id=\"map-title-desc-bar\">\n" +
-                    "                            <div style=\"padding: 300px 32px;\">\n" +
-                    "                                <div style=\"padding-left: 53px; padding-right: 53px;\">\n" +
-                    "                                    <svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" style=\"height: 101px; width: 101px; fill: #cdcdcd; cursor: pointer\"\n" +
-                    "                                         onmouseover=\"this.style.fill='#444444'\" onmouseout=\"this.style.fill='#cdcdcd'\" onclick=\"console.log('todo: 添加行程')\">\n" +
-                    "                                        <g>\n" +
-                    "                                            <path fill=\"none\" d=\"M0 0h24v24H0z\"/>\n" +
-                    "                                            <path d=\"M4 3h16a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1zm1 2v14h14V5H5zm6 6V7h2v4h4v2h-4v4h-2v-4H7v-2h4z\"/>\n" +
-                    "                                        </g>\n" +
-                    "                                    </svg>\n" +
-                    "                                </div>\n" +
-                    "                                <div id=\"map-title-desc-bar-name\" class=\"map-title-desc-bar-nameClass\" style=\"width: 207px; height: 23px; text-align: center\">立即创建属于你的行程</div>\n" +
-                    "                            </div>\n" +
-                    "                        </div>\n" +
-                    "                    </div>\n" +
-                    "                </div>";
+                let pane = `<div id="featurelist-titlebar-container" style="height: 763px">
+                                    <div id="featurelist-title-bar" class="featurelist-title-barClass">
+                                            <div id="map-title-desc-bar">
+                                                <div style="padding: 300px 32px;">
+                                                    <div style="padding-left: 53px; padding-right: 53px;">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="height: 101px; width: 101px; fill: #cdcdcd; cursor: pointer"
+                                                             onmouseover="this.style.fill='#444444'" onmouseout="this.style.fill='#cdcdcd'" onclick="console.log('todo: 添加行程')">
+                                                            <g>
+                                                                <path fill="none" d="M0 0h24v24H0z"/>
+                                                                <path d="M4 3h16a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1zm1 2v14h14V5H5zm6 6V7h2v4h4v2h-4v4h-2v-4H7v-2h4z"/>
+                                                            </g>
+                                                        </svg>
+                                                    </div>
+                                                    <div id="map-title-desc-bar-name" class="map-title-desc-bar-nameClass" style="width: 207px; height: 23px; text-align: center">立即创建属于你的行程</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>`;
                 if (document.getElementById('featurelist-pane')) {
                     document.getElementById('featurelist-pane').innerHTML = pane;
                 } else {
                     console.log("getElementById featurelist-pane failed");
                 }
             } else {
+                defaultStroke = data.data["strokes_info"]["default_stroke"];
                 setUserMarket(data.data["strokes_info"]["default_stroke"]["point_list"]);
                 let pane = bindStrokeInfo(data.data["strokes_info"]);
                 if (document.getElementById('featurelist-pane')) {
@@ -69,64 +70,70 @@ let makeGetUserInfoRequest = function() {
 
 let bindStrokeInfo = function(strokeList) {
     let pane;
-    pane = "<div id=\"featurelist-titlebar-container\">\n" +
-        "                    <div id=\"featurelist-title-bar\" class=\"featurelist-title-barClass\">\n" +
-        "                        <div id=\"map-action-menu\" class=\"map-action-menuClass\"></div>\n" +
-        "                        <div id=\"map-title-desc-bar\">\n" +
-        "                            <div id=\"map-title-desc-bar-name\" class=\"map-title-desc-bar-nameClass\">default itinerary</div>\n" +
-        "                            <div id=\"map-title-desc-bar-time\" class=\"map-title-desc-bar-timeClass\">\n" +
-        "                                <div>上次修改时间: " + strokeList["default_stroke"]["update_time"] + "</div>\n" +
-        "                            </div>\n" +
-        "                        </div>\n" +
-        "                        <ul class=\"map-actionClass\">\n" +
-        "                            <div style=\"height: 16px\"></div>\n" +
-        "                        </ul>\n" +
-        "                    </div>\n" +
-        "                </div>\n" +
-        "                <div id=\"featurelist-scrollable-container\" style=\"max-height: 625px; max-width: 307px; overflow-x: hidden; overflow-y: auto\">\n" +
-        "                    <div class=\"stroke-info\">\n" +
-        "                        <div class=\"point-list\" style=\"position: relative\">\n" +
-        "                            <div class=\"point-list-layer\">\n" +
-        "                                <div class=\"point-list-layer-header\">\n" +
-        "                                    <div class=\"point-list-layer-header-font\">行程点集</div>\n" +
-        "                                </div>\n" +
-        "                                <div class=\"point-list-layer-body\">\n" +
-        "                                    <div class=\"point-list-layer-body-container\">";
+    pane = `<div id="featurelist-titlebar-container">
+                    <div id="featurelist-title-bar" class="featurelist-title-barClass">
+                        <div id="map-action-menu" class="map-action-menuClass"></div>
+                        <div id="map-title-desc-bar">
+                            <div id="map-title-desc-bar-name" class="map-title-desc-bar-nameClass">default itinerary</div>
+                            <div id="map-title-desc-bar-time" class="map-title-desc-bar-timeClass">
+                                <div>上次修改时间: ${strokeList["default_stroke"]["update_time"]}</div>
+                            </div>
+                        </div>
+                        <ul class="map-actionClass">
+                            <div style="height: 16px"></div>
+                        </ul>
+                    </div>
+                </div>
+                <div id="featurelist-scrollable-container" style="max-height: 625px; max-width: 307px; overflow-x: hidden; overflow-y: auto">
+                    <div class="stroke-info">
+                        <div class="point-list" style="position: relative">
+                            <div class="point-list-layer">
+                                <div class="point-list-layer-header">
+                                    <div class="point-list-layer-header-font">行程点集</div>
+                                </div>
+                                <div class="point-list-layer-body">
+                                    <div class="point-list-layer-body-container">`;
 
     for(let point of strokeList["default_stroke"]["point_list"]) {
-        pane += "<div class=\"point-list-layer-body-item\">\n" +
-            "                                            <div class=\"point-logo\">\n" +
-            "                                                <div class=\"point-logo-svg\" style=\"background-image:url(https://mt.google.com/vt/icon/name=icons/onion/SHARED-mymaps-pin-container_4x.png,icons/onion/1899-blank-shape_pin_4x.png&highlight=0288D1,ff000000&scale=2.0); background-position:center; background-size:contain;\" iconcode=\"1899-0288D1\"></div>\n" +
-            "                                            </div>\n" +
-            "                                            <div class=\"point-font-container\">\n" +
-            "                                                <div class=\"point-font\" onclick=''>" + point["text"] + "</div>\n" +
-            "                                            </div>\n" +
-            "                                        </div>"
+        pane += `<div class="point-list-layer-body-item">
+                                            <div class="point-logo">
+                                                <div class="point-logo-svg" style="background-position:center; background-size:contain;" iconcode="1899-0288D1">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="fill: #0052CC"><g>
+                                                            <path fill="none" d="M0 0h24v24H0z"/>
+                                                            <path d="M12 23.728l-6.364-6.364a9 9 0 1 1 12.728 0L12 23.728zm4.95-7.778a7 7 0 1 0-9.9 0L12 20.9l4.95-4.95zM12 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"/>
+                                                        </g>
+                                                    </svg>
+                                                </div>
+                                            </div>
+                                            <div class="point-font-container">
+                                                <div class="point-font" onclick="selectUserMarket('${point["point_id"]}', '${point["point_type"]}')">${point["text"]}</div>
+                                            </div>
+                                        </div>`;
     }
 
-    pane += "</div>\n" +
-        "                                </div>\n" +
-        "                            </div>\n" +
-        "                        </div>\n" +
-        "                        <div class=\"point-list\" style=\"position: relative\">\n" +
-        "                            <div class=\"point-list-layer\">\n" +
-        "                                <div class=\"point-list-layer-header\">\n" +
-        "                                    <div class=\"point-list-layer-header-font\">行程路线集</div>\n" +
-        "                                </div>\n" +
-        "                            </div>\n" +
-        "                        </div>\n" +
-        "                        <ul class=\"map-actionClass\">\n" +
-        "                            <div style=\"height: 0\"></div>\n" +
-        "                        </ul>\n" +
-        "                        <div class=\"point-list\" style=\"position: relative\">\n" +
-        "                            <div class=\"point-list-layer\">\n" +
-        "                                <div class=\"point-list-layer-header\">\n" +
-        "                                    <div class=\"point-list-layer-header-font\">历史行程</div>\n" +
-        "                                </div>\n" +
-        "                            </div>\n" +
-        "                        </div>\n" +
-        "                    </div>\n" +
-        "                </div>";
+    pane += `</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="point-list" style="position: relative">
+                            <div class="point-list-layer">
+                                <div class="point-list-layer-header">
+                                    <div class="point-list-layer-header-font">行程路线集</div>
+                                </div>
+                            </div>
+                        </div>
+                        <ul class="map-actionClass">
+                            <div style="height: 0"></div>
+                        </ul>
+                        <div class="point-list" style="position: relative">
+                            <div class="point-list-layer">
+                                <div class="point-list-layer-header">
+                                    <div class="point-list-layer-header-font">历史行程</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
 
     return pane
 };
